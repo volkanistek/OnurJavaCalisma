7ec9980b474b2ebeca542353001ea584b422a1ac
volkan istek <volkanistek@mail.com>
2025-03-02 16:29:34 +0530

    update

--- src/net/javaci/volkan/BubbleSort.java
+++ src/net/javaci/volkan/BubbleSort.java
@@ -5,24 +5,21 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] array = {4, 9, 1, 0, 13, 7};
+
+        int[] array = {4, 9, 1, 0, 13, 7, 9};
+
         System.out.println(Arrays.toString(array));
-        sort(array);
-    }
 
-    public static void sort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 0; j < array.length - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
 
-
-/*
-    1 for (int k = 1; k < array.length; k++) {
-    2   // Perform the kth pass
-    3   for (int i = 0; i < array.length - k; i++) {
-    4       if (list[i] > list[i + 1])
-    5           swap array[i] with array[i + 1];
-    6   }
-    7 }
- */
-
-
+        System.out.println(Arrays.toString(array));
     }
 }
